2d38907cec606fd0b95420dd35255a9a078f204d
renattos <renattos@gmail.com>
2018-04-03 17:02:45 +0000

    add ;

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -18,7 +18,7 @@ public class HttpApplication extends AbstractVerticle {
     // Create a router object.
     Router router = Router.router(vertx);
 
-    router.get("/api/greeting").handler(this::greeting)
+    router.get("/api/greeting").handler(this::greeting);
     router.get("/*").handler(StaticHandler.create());
 
     // Create the HTTP server and pass the "accept" method to the request handler.
